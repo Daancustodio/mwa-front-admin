56d7e8ebacddf4d871568261b8713cbc81f2e65f
Andre Baltieri <andrebaltieri@hotmail.com>
2015-08-01 15:43:47 -0300

    Iniciando repo

--- app/config.js
+++ app/config.js
@@ -5,8 +5,8 @@
         "CURR_ENV": "dev",
         "AUTH_TOKEN": "mwa-token",
         "AUTH_USER": "mwa-user",
-        "SERVICE_URL": "http://abt-mwa-api.azurewebsites.net/"
-        //"SERVICE_URL": "http://localhost:3348/"
+        //"SERVICE_URL": "http://abt-mwa-api.azurewebsites.net/"
+        "SERVICE_URL": "http://localhost:51240"
     });
 
     angular.module('mwa').run(function ($rootScope, $location, SETTINGS) {
